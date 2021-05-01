f1abc4526655e1fcfc5e145bb32a6d95374c0c2c
saurabh241930 <sp241930@gmail.com>
2021-05-01 19:00:47 +0530

    m

--- mail_utils.py
+++ mail_utils.py
@@ -10,7 +10,12 @@ from datetime import date
 from dateutil.relativedelta import relativedelta, SU
 today = datetime.today()
 last_monday = today + relativedelta(weekday=SU(-1))
-
+from datetime import date
+from dateutil.relativedelta import relativedelta, SU
+today = datetime.today()
+yesterday = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), '%Y-%m-%d') -timedelta(days=1)
+last_monday = today + relativedelta(weekday=SU(-1))
+current_month_text = datetime.now().strftime('%B')
 
 import pymongo
 client = pymongo.MongoClient("mongodb+srv://dbChirag:dbChirag123@swatiprodcluster0.bmxsv.mongodb.net/swatiinteriors")
@@ -31,26 +36,12 @@ imeis = db['imeis']
 
 
 
-def get_daily_footfall(user_site_preferences):
-    today_date = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), '%Y-%m-%d')
-    user_site_preferences = ['INDIABULLS BLU-PENT HOUSE', 'DLF LIMITED']
-    total_footfall_query = [{'$match': {'date': today_date,'site_name':{'$in':user_site_preferences}}},
-                                {"$group": {'_id': '$site_name',
-                                            'total_worker_footfall': {'$sum': 1},
-                                            }}]
-
-    total_footfall_project_wise_data = attendances.aggregate(total_footfall_query)
-    return list(total_footfall_project_wise_data)
-
-
-
-def convert_to_HTML(element_html):
+def report_template(element_html):
 
 
 
     styles = '''
 
-            <style>
             #customers {
             font-family: Arial, Helvetica, sans-serif;
             border-collapse: collapse;
@@ -73,7 +64,6 @@ def convert_to_HTML(element_html):
             background-color: black;
             color: white;
             }
-            </style>
 
             '''
 
@@ -81,8 +71,10 @@ def convert_to_HTML(element_html):
     <!DOCTYPE html>
     <html>
     <head>
+    
+    <style>{}</style>
 
-    {}
+    
     
     <title>Page Title</title>
     </head>
@@ -92,13 +84,11 @@ def convert_to_HTML(element_html):
 
     <h2>SWATI INTERIOR</h2>
 
-    <p>This mail is from SWATI INTERIOR erp management software </p>
-
-    <h1>1)DAILY FOOTFALL:</h1>
+    <p>This mail is consist of software generated weekly summarized attendance report from SWATI INTERIOR erp management software,you will recieve this auto-generated at every end of the week of your corresponding project</p>
 
     {}
 
-    <p>Please do not reply</p>
+    <p>More features incoming soon</p>
 
     </body>
     </html>
@@ -118,35 +108,36 @@ def convert_to_HTML_table(data,column_names):
     for d in data:
         parent_row = ""
         for i in d.keys():
+            d[i] = int(d[i]) if type(d[i]) == float else d[i]
             parent_row = parent_row + "<td>"+str(d[i])+"</td>"
         table_body = table_body + "<tr>"+ parent_row + "</tr>"
 
     table_HTML = '<table id="customers">{}{}</table>'.format(headers_element,table_body)
 
-    return convert_to_HTML(table_HTML)
+    return table_HTML
 
 
 
 
-def get_yesterday_footfall(user_site_preferences):
-    today_date = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), '%Y-%m-%d') -timedelta(days=1)
-    user_site_preferences = ['INDIABULLS BLU-PENT HOUSE', 'DLF LIMITED']
-    total_footfall_query = [{'$match': {'date': today_date,'site_name':{'$in':user_site_preferences}}},
-                                {"$group": {'_id': '$site_name',
-                                            'total_worker_footfall': {'$sum': 1},
-                                            'average_working_hour_onsite': {'$avg': '$duration'}
-                                            }}]
-
-    total_footfall_project_wise_data = attendances.aggregate(total_footfall_query)
-    return list(total_footfall_project_wise_data)
-
-
-def get_today_footfall(user_site_preferences):
+def get_daily_footfall(user_site_preferences):
     today_date = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), '%Y-%m-%d')
     user_site_preferences = ['INDIABULLS BLU-PENT HOUSE', 'DLF LIMITED']
     total_footfall_query = [{'$match': {'date': today_date,'site_name':{'$in':user_site_preferences}}},
                                 {"$group": {'_id': '$site_name',
                                             'total_worker_footfall': {'$sum': 1},
+                                            }}]
+
+    total_footfall_project_wise_data = attendances.aggregate(total_footfall_query)
+    return list(total_footfall_project_wise_data)
+
+
+
+
+
+def get_yesterday_footfall(user_site_preferences):
+    total_footfall_query = [{'$match': {'date': yesterday,'site_name':{'$in':user_site_preferences}}},
+                                {"$group": {'_id': '$site_name',
+                                            'total_worker_footfall': {'$sum': 1},
                                             'average_working_hour_onsite': {'$avg': '$duration'}
                                             }}]
 
@@ -154,7 +145,34 @@ def get_today_footfall(user_site_preferences):
     return list(total_footfall_project_wise_data)
 
 
-def get_week_footfall(user_site_preferences):
+def get_yesterday_blacklist(user_site_preferences):
+    today = datetime.today()
+    query = [{'site_name':{'$in':user_site_preferences},'attendance_points':None,'date': yesterday}]
+    return list(attendances.find(query))
+
+
+def pre_process_footfall_history(footfall_list):
+    out = list()
+    for i in footfall_list:
+        i["average_working_hour_onsite"] = "No data available" if i["average_working_hour_onsite"] == None else str(int(i["average_working_hour_onsite"]))
+        out.append(i)
+        
+    return out
+    
+
+def get_today_footfall(user_site_preferences):
+    today_date = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), '%Y-%m-%d')
+    total_footfall_query = [{'$match': {'date': today_date,'site_name':{'$in':user_site_preferences}}},
+                                {"$group": {'_id': '$site_name',
+                                            'total_worker_footfall': {'$sum': 1},
+                                            'average_working_hour_onsite': {'$avg': '$duration'}
+                                            }}]
+
+    total_footfall_project_wise_data = attendances.aggregate(total_footfall_query)
+    return list(total_footfall_project_wise_data)
+
+
+def get_all_week_footfall(user_site_preferences):
     
     total_footfall_query = [{'$match': {'site_name':user_site_preferences[0]}},
                                 {"$group": {'_id':{'$week':'$date'},
@@ -167,20 +185,34 @@ def get_week_footfall(user_site_preferences):
 
 
 
+def get_week_footfall(user_site_preferences):
+    out = list()
+    for s in user_site_preferences:
+    
+        query = [{'$match': {'site_name':s,'date': {"$gte": last_monday, "$lte": today}}},
+                                    {"$group": {'_id':{'$dateToString': { 'format': "%Y-%m-%d", 'date': "$date"}},
+                                                'total_worker_footfall': {'$sum': 1},
+                                                'average_working_hour_onsite': {'$avg': '$duration'},
+                                                }},{'$sort':{'_id':1}}]
+        
+        out.append({str(s):list(attendances.aggregate(query))})
+
+    return out
+
 
 def get_users_with_null_attendance_weekly_blacklist(user_site_preferences):
-    
-    total_footfall_query = [{'$match': {'attendance_points':None,
-                                        'date': {"$gte": last_monday, "$lte": today},
-                                        'site_name':user_site_preferences[0]}},
-                                {"$group": {'_id': '$worker_full_name',
-                                            'no_of_times_missed_attendance': {'$sum': 1}
-                                            }},{'$sort':{'date':-1}}]
+    out = []
+    for s in user_site_preferences:
+        query = [{'$match': {'attendance_points':None,
+                                            'date': {"$gte": last_monday, "$lte": today},
+                                            'site_name':s}},
+                                    {"$group": {'_id': '$worker_full_name',
+                                                'no_of_times_missed_attendance': {'$sum': 1}
+                                                }},{'$sort':{'date':-1}}]
 
-    total_footfall_project_wise_data = attendances.aggregate(total_footfall_query)
-    return list(total_footfall_project_wise_data)
-
-
+        out.append({s:list(attendances.aggregate(query))})
+        
+    return out
 
 
 def get_diff_weekly(user_site_preferences):
@@ -194,29 +226,62 @@ def get_diff_weekly(user_site_preferences):
                                                 }},{'$sort':{'_id':-1}}]
 
         weekly_footfalls = list(attendances.aggregate(total_footfall_query))
-    
-    
-        
-        tw,pw = weekly_footfalls[0]['total_worker_footfall'],weekly_footfalls[1]['total_worker_footfall']
-        footfall_status = "dropped" if pw > tw else "hiked"
-        report.append({"site":s,
-                       "previous_week_footfall":pw,
-                       "this_week_footfall":tw,
-                       "footfall_status":footfall_status,
-                       "change":float(((tw-pw)*100)/pw)})
-                      
+        try:
+            tw,pw = weekly_footfalls[0]['total_worker_footfall'],weekly_footfalls[1]['total_worker_footfall']
+            footfall_status = "dropped" if pw > tw else "hiked"
+            report.append({"site":s,
+                           "previous_week_footfall":pw,
+                           "this_week_footfall":tw,
+                           "footfall_status":footfall_status,
+                           "change":float(((tw-pw)*100)/pw)})
+        except IndexError:
+            continue   
     return report
+
+
+
+
+
+def gen_week_summary(user_site_preferences):
     
+    week_summary = get_diff_weekly(user_site_preferences)
+    week_blacklist_list = get_users_with_null_attendance_weekly_blacklist(user_site_preferences)
+    week_footfall = get_week_footfall(user_site_preferences)
+    
+    out = "<h1>WEEKLY ATTENDANCE SUMMARY REPORT</h1><br><br"
+    
+    out = out + "<p>1).Below is the list of comparison of this week footfall & its previous week footfall of total footfall along with dropped/hiked percentage</p>"
+    week_summary_table_HTML = convert_to_HTML_table(week_summary,
+                                              column_names=["Site","Footfall(last week)",
+                                                            "Footfall(this week)","Comparison Indicator","Net change in %"])
+    out = out+"<br><br>"
+    out = out + week_summary_table_HTML
+    out = out + "<p style='font-size:23px;color:red'>2).Below is the list of all individual worker who has has note completed attendance across sites</p>"
 
-
-
-
-
-
-
-
-  
-
-
-
+    for l in week_blacklist_list:
         
+        key = list(l.keys())[0]
+        if len(l[key]) == 0:
+            continue
+        out = out + "<p><strong><span style='color:red;font-size:21px'>{}:<br></span></strong>List of worker with incompleted attendance</p>".format(key)
+        week_blacklist_table_HTML = convert_to_HTML_table(l[key],column_names=["Worker/Employee name","No. of times attendance not completed(this week)"])
+        out = out + week_blacklist_table_HTML
+        
+    out = out+"<br><br>"
+        
+    out = out + "<p style='font-size:23px;color:blue'>3).Following is the list of footfall over this week along with average shift hour of that specific day in week</p>"
+    for l in week_footfall:
+        key = list(l.keys())[0]
+        if len(l[key]) == 0:
+            out = out + "<p><strong><span style='color:blue;font-size:21px'>{}:<br></span></strong>No data recorded for this site this week</p>".format(key)
+            continue
+        out = out + "<p><strong><span style='color:blue;font-size:21px'>{}:<br></span></strong>Footfall history over this week</p>".format(key)
+        week_footfall_table_HTML = convert_to_HTML_table(pre_process_footfall_history(l[key]),
+                                                          column_names=["Week Date","Footfall","Average shift hour duration"])
+        out = out + week_footfall_table_HTML
+        
+    out = out+"<br><br>"
+    
+    
+    return report_template(out)
+    
--- send_mail.py
+++ send_mail.py
@@ -1,7 +1,7 @@
 # importing libraries
 from flask import Flask
 from flask_mail import Mail, Message
-from mail_utils import get_daily_footfall,convert_to_HTML_table
+from mail_utils import gen_week_summary
 
 
 app = Flask(__name__)
@@ -31,7 +31,8 @@ def create_report_email(report_type,report_frequency,user_preference_sites):
 
 
     return output
-    
+
+
 
 # message object mapped to a particular URL ‘/’
 @app.route("/")
@@ -39,13 +40,37 @@ def index():
     msg = Message(
     'SWATI INTERIOR ERP automated report system',
     sender ='sherlindhubia@gmail.com',
-    recipients = ['sp241930@gmail.com']
-    # recipients=["herezeon@gmail.com","sp241930@gmail.com","chiragprajapati781@gmail.com","yash@swatiinterior.com "]
+    recipients = ['ap241931@gmail.com']
+    # recipients=["herezeon@gmail.com","sp241930@gmail.com","chiragprajapati781@gmail.com","yash@swatiinterior.com ","nkmistry@swatiinterior.com","ashok@swatiinterior.com"]
     )
+
+    user_site_preferences = ['360 WEST',
+    'BHILOSA INDUSTRIES',
+    'BIRLA CENTURION',
+    'CHATURVEDI BUNGLOW-ALIBAUG',
+    'DLF LIMITED',
+    'GODREJ TWO',
+    'HYDE PARK',
+    'INDIABULLS 20TH FLOOR',
+    'INDIABULLS 44 & 45 floor',
+    'INDIABULLS BLU-PENT HOUSE',
+    'INDIABULLS CLUB HOUSE',
+    'JWC RETAIL',
+    'JWC-CEC',
+    'KARUNA SINDHU',
+    'LEJEND',
+    'MAKER CHAMBER',
+    'MARRIOT ',
+    'MR. ASHOK PILLAI ',
+    'NEHRU CENTER',
+    'PRITESH SHAH -JUHU',
+    'QUEENS PARK']
+
+    msg.html = gen_week_summary(user_site_preferences)
 
     # print(create_report_email("ATTENDANCE","DAILY",user_preference_sites=['DLF LIMITED','INDIABULLS BLU-PENT HOUSE']))
 
-    msg.html = create_report_email("ATTENDANCE","DAILY",user_preference_sites=['DLF LIMITED','INDIABULLS BLU-PENT HOUSE'])
+    # msg.html = create_report_email("ATTENDANCE","DAILY",user_preference_sites=['DLF LIMITED','INDIABULLS BLU-PENT HOUSE'])
     
     # with app.open_resource("chart.png") as fp:
     #     msg.attach("chart.png", "image/png", fp.read())
